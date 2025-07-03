7e12ebbe5f0c67203625b2a2ab90080d8c129e5e
Randy Wind <wind.randy@gmail.com>
2025-07-04 00:53:05 +0200

    Refactor attempt, move and rename VActCuDNN test functions

---
+++ VActCuDNN/Source/VActCuDNN/Public/_VActCuDNN_Tests.h
@@ -0,0 +1,14 @@
+#if WITH_EDITOR 
+struct _FVActCuDNNTests
+{
+	static void _Debug_Cuda_Test();
+
+	static void _Debug_CuDNN_Test_0();
+
+	static void _Debug_CuDNN_Test_1();
+
+	static void _Debug_VActCuDNN_Test_0();
+
+	static void _Debug_VActCuDNN_Test_1();
+};
+#endif;
