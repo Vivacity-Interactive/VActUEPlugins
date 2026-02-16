df4d733d2e59d4cc9ae95132c2aef32142fd4a90
Randy Wind <wind.randy@gmail.com>
2026-02-16 02:28:21 +0100

    work in progress (wip) VActBeats plugin, not yet compiling

--- VActMath/Source/VActMath/Private/VActMathTypes.cpp
+++ VActMath/Source/VActMath/Private/VActMathTypes.cpp
@@ -1,2 +1,15 @@
 #include "VActMathTypes.h"
 
+FWeightedEntry::FWeightedEntry()
+	: Index(-1)
+	, Weight(1.0f)
+{
+
+}
+
+FWeightedEntry::FWeightedEntry()
+	: Index(-1)
+	, Weight(1.0f)
+{
+
+}
--- VActMath/Source/VActMath/Public/VActMathTypes.h
+++ VActMath/Source/VActMath/Public/VActMathTypes.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "VActMathTypes.generated.h"
 
 UENUM()
 enum class EMathOperation
@@ -101,3 +102,108 @@ struct FVActMathConst
 	template<typename T0>
 	static constexpr T0 Min{ TNumericLimits<T0>::Min() };
 };
+
+template<typename T0, uint32 N0>
+struct TMathVector
+{
+	T0 Data[N0];
+
+	FORCEINLINE int32 Num() const
+	{
+		return N0;
+	}
+
+	FORCEINLINE SIZE_T NumBytes() const
+	{
+		return sizeof(Data);
+	}
+
+	FORCEINLINE bool IsValidIndex(int32 Index) const
+	{
+		return Index >= 0 && Index < static_cast<int32>(N0);
+	}
+
+	FORCEINLINE void RangeCheck(int32 Index) const
+	{
+		checkf((Index >= 0) & (Index < static_cast<int32>(N0)), TEXT("MathVector index out of bounds: %lld into an array of size %lld"), (long long)Index, (long long)NumFeatures);
+	}
+
+	FORCEINLINE operator T0* ()
+	{
+		return Data;
+	}
+
+	FORCEINLINE operator const T0* () const
+	{
+		return Data;
+	}
+
+	FORCEINLINE T0& operator[](int32 Index)
+	{
+		RangeCheck(Index);
+		return Data[Index];
+	}
+
+	FORCEINLINE const T0& operator[](int32 Index) const
+	{
+		RangeCheck(Index);
+		return Data[Index];
+	}
+};
+
+template<uint32 N0>
+using TMathEffector = TMathVector<EMathOperation, N0>;
+
+template<typename T0, uint32 N0>
+struct TMathMonoEffect
+{
+	EMathOperation Effector;
+	TMathVector<T0, N0> Vector;
+};
+
+template<typename T0, uint32 N0>
+struct TMathEffect
+{
+	TMathEffector<N0> Effector;
+	TMathVector<T0, N0> Vector;
+};
+
+template<typename T0>
+struct TWeightedEntry
+{
+	T0 Index;
+	float Weight;
+};
+
+USTRUCT(BlueprintType, Blueprintable)
+struct VACTMATH_API FWeightedEntry
+{
+	GENERATED_BODY()
+
+	int32 Index;
+	float Weight;
+
+	FWeightedEntry();
+};
+
+
+template<typename T0, typename TypeMathVector>
+using TMathVectorOf = TMathVector<sizeof(TypeMathVector) / sizeof(T0)>;
+
+template<typename T0, typename TypeMathVector>
+using TMathEffectorOf = TMathEffector<sizeof(TypeMathVector) / sizeof(T0)>;
+
+template<typename T0, typename TypeMathVector>
+using TMathEffectOf = TMathEffect<T0, sizeof(TypeMathVector) / sizeof(T0)>;
+
+template<typename T0, typename TypeMathVector>
+using TMathMonoEffectOf = TMathMonoEffect<T0, sizeof(TypeMathVector) / sizeof(T0)>;
+
+template<uint32 N0>
+using TFloatMathVector = TMathVector<float, N0>;
+
+template<uint32 N0>
+using TFloatMathEffect = TMathEffect<float, N0>;
+
+template<uint32 N0>
+using TFloatMathMonoEffect = TMathMonoEffect<float, N0>;
